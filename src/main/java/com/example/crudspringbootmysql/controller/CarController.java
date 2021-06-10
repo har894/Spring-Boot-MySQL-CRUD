037605bea1e1602cc065a7bac428bcc2e75c8ec7
sevakivanyan <sevakivanyan@gmail.com>
2021-06-10 13:59:36 +0400

    Add find by name

--- src/main/java/com/example/crudspringbootmysql/controller/CarController.java
+++ src/main/java/com/example/crudspringbootmysql/controller/CarController.java
@@ -2,8 +2,10 @@ package com.example.crudspringbootmysql.controller;
 
 import com.example.crudspringbootmysql.entity.Car;
 import com.example.crudspringbootmysql.service.CarService;
+import jdk.jfr.Name;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,4 +19,9 @@ public class CarController {
     public List<Car> getCars() {
         return carService.getCars();
     }
+
+    @GetMapping("/Car/{name}")
+    public Car getCarName(@PathVariable String name ){return carService.getCarName(name);};
 }
+
+

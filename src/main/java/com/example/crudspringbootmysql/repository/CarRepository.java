6309c7273d8852a4d0642927368eeb65d7a2e903
aram <artsvaberd21@gmail.com>
2021-06-15 00:59:24 +0400

    Deleted the function find car by speed and  copy of the function   find car by id

--- src/main/java/com/example/crudspringbootmysql/repository/CarRepository.java
+++ src/main/java/com/example/crudspringbootmysql/repository/CarRepository.java
@@ -4,7 +4,5 @@ import com.example.crudspringbootmysql.entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CarRepository extends JpaRepository<Car, Integer> {
-
-    Car findBySpeed(Integer speed
-    Car findByName(String name)
+    Car findByName(String name);
 }

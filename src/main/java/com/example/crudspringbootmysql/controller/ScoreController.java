59c856fb4b13006a8bbafc12ed9407b2aefaf2c0
sevakivanyan <sevakivanyan@gmail.com>
2021-06-21 16:43:50 +0400

    I opened the CROSS origin access and removed unnecessary imports.

--- src/main/java/com/example/crudspringbootmysql/controller/ScoreController.java
+++ src/main/java/com/example/crudspringbootmysql/controller/ScoreController.java
@@ -1,30 +1,24 @@
 package com.example.crudspringbootmysql.controller;
 
-import com.example.crudspringbootmysql.entity.Car;
 import com.example.crudspringbootmysql.entity.Score;
-import com.example.crudspringbootmysql.service.CarService;
 import com.example.crudspringbootmysql.service.ScoreService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
+@CrossOrigin
 @RestController
 public class ScoreController {
     @Autowired
     private ScoreService scoreService;
 
-    @GetMapping("/scoreId/{id}")
+    @GetMapping("/gameId/{id}")
     public Score findScoreByGameId(@PathVariable int id) {
         return scoreService.getScoreByGameId(id);
     }
 
     @PostMapping("/increment/{id}/{teamNumber}")
     public Score incrementScore(@PathVariable int id, @PathVariable int teamNumber) {
-        return scoreService.incrementScore(id,teamNumber);
+        return scoreService.incrementScore(id, teamNumber);
     }
 }
 

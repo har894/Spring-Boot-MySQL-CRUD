10bd91076578546f7b073dd3be3ea4f53014c5c7
Harut Sahakyan <hsahakyan94@gmail.com>
2021-11-19 23:20:17 +0400

    Configured template engine.

---
+++ src/main/java/com/example/crudspringbootmysql/controller/DashboardController.java
@@ -0,0 +1,24 @@
+package com.example.crudspringbootmysql.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping({"/ui"})
+public class DashboardController {
+
+    @GetMapping("/dashboard")
+    public String dashboard(Model model) {
+
+        return "dashboard";
+    }
+
+    @GetMapping("/")
+    public String main(Model model) {
+
+        return "index";
+    }
+
+}
